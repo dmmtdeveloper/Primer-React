e401f309d5a5e74c0ad9befc38a6b95c8e9dcb8d
David <125302548+dmmtdeveloper@users.noreply.github.com>
2023-10-02 21:32:09 -0300

    final

---
+++ my-react-app/src/components/Componente.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+function component(){
+  return(
+    <Fragment>
+      <div>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    </Fragment>
+  )
+}
